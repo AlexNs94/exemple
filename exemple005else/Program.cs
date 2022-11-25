0c88224ef847752f77ea930f3c506e89cd45b84e
alexns <alexns9419@yandex.ru>
2022-11-25 23:46:30 +0500

    exemple005else

---
+++ exemple005else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя потзователя : ");
+string username = Console.ReadLine ();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine ("Ураа, это же Маша!!");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
